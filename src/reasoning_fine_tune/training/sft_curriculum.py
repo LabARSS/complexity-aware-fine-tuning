Fix metric computation

src/reasoning_fine_tune/training/sft_curriculum.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 import pandas as pd
 import torch
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from transformers.data.data_collator import DataCollatorForSeq2Seq
 from transformers.trainer import Trainer
 from transformers.trainer_callback import TrainerState
@@ -14,15 +13,18 @@ from reasoning_fine_tune.utils.prepare_dataset_for_training import prepare_datas
 
 BATCH_SIZE = 4
 
-
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = torch.argmax(predictions, dim=-1)
-    acc, prec, rec, f1 = (
-        accuracy_score(labels, predictions),
-        *precision_recall_fscore_support(labels, predictions, average="binary")[:3],
-    )
-    return {"acc": acc, "prec": prec, "rec": rec, "f1": f1}
+class ArgmaxTrainer(Trainer):
+    def prediction_step(self, model, inputs, prediction_loss_only, ignore_keys=None):
+        loss, logits, labels = super().prediction_step(
+            model,
+            inputs,
+            prediction_loss_only=False,      # we need logits once
+            ignore_keys=ignore_keys,
+        )
+        # keep only the class ids; still keep sequence dim if you need one-per-token
+        if logits is not None:
+            logits = logits.argmax(dim=-1).to(torch.int16)  # tiny!
+        return loss, logits, labels
 
 
 def get_sys_prompt(row):
@@ -39,6 +41,22 @@ def get_user_prompt(row):
 def train_sft_curriculum(
     name, model, tokenizer, easy_train_df_path, mid_train_df_path, hard_train_df_path, test_df_path
 ):
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+
+        total = labels.shape[0]
+
+        target_mask = (labels != -100) & (labels != tokenizer.eos_token_id)
+        correct_tokens = (predictions == labels) & target_mask
+
+        # We need to account for correct answers only all tokens matched.
+        # For instance, 11 could be encoded as two tokens X.
+        # Then if model replies 1 (X,), but we expect 11 (X,X), we will count the answer as half-correct, which is wrong
+        correct_batches_mask = correct_tokens.sum(-1) == target_mask.sum(-1)
+        correct_batches = correct_batches_mask.sum()
+
+        return {"accuracy": correct_batches / total }
+
     easy_train_df = pd.read_csv(
         easy_train_df_path,
         sep="\t",
@@ -117,8 +135,9 @@ def train_sft_curriculum(
         overwrite_output_dir=True,
         save_total_limit=1,
         save_only_model=True,
+        eval_on_start=True
     )
-    trainer = Trainer(
+    trainer = ArgmaxTrainer(
         model=model,
         args=training_args,
         train_dataset=easy_train_ds,
@@ -126,6 +145,7 @@ def train_sft_curriculum(
         data_collator=data_collator,
         compute_metrics=compute_metrics,
     )
+
     trainer.train()
 
     training_args.output_dir = str(mid_output_dir)