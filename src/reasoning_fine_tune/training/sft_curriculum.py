760776e77dea42b0c4f7c7bd5c8c109d445cefb6
Andrey Goncharov <andrey@goncharov.page>
2025-06-30 14:14:03 +0300

    Extract stage training

--- src/reasoning_fine_tune/training/sft_curriculum.py
+++ src/reasoning_fine_tune/training/sft_curriculum.py
@@ -62,12 +62,7 @@ def get_user_prompt(row):
     return prompts.single_token_answer_prompt_with_fallback_for_unknown_answers(question, options)
 
 
-def train_sft_curriculum(name, model_id, easy_train_df_path, mid_train_df_path, hard_train_df_path, test_df_path):
-    np.random.seed(42)
-    torch.manual_seed(42)
-
-    print(f"Using device: {DEVICE_MAP}")
-
+def train_sft_curriculum_stage(output_subpath, model_id, train_df_path, test_df_path, num_train_epochs, eval_on_start=False):
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     def compute_metrics(eval_pred):
@@ -83,18 +78,8 @@ def train_sft_curriculum(name, model_id, easy_train_df_path, mid_train_df_path, 
 
         return {"accuracy": accuracy}
 
-    easy_train_df = pd.read_csv(
-        easy_train_df_path,
-        sep="\t",
-        header=0,
-    )
-    mid_train_df = pd.read_csv(
-        mid_train_df_path,
-        sep="\t",
-        header=0,
-    )
-    hard_train_df = pd.read_csv(
-        hard_train_df_path,
+    train_df = pd.read_csv(
+        train_df_path,
         sep="\t",
         header=0,
     )
@@ -111,36 +96,22 @@ def train_sft_curriculum(name, model_id, easy_train_df_path, mid_train_df_path, 
         sep="\t",
         header=0,
     )
-    easy_train_df = pd.merge(mmlu_df, easy_train_df["question_id"], on="question_id", how="inner")
-    mid_train_df = pd.merge(mmlu_df, mid_train_df["question_id"], on="question_id", how="inner")
-    hard_train_df = pd.merge(mmlu_df, hard_train_df["question_id"], on="question_id", how="inner")
+    train_df = pd.merge(mmlu_df, train_df["question_id"], on="question_id", how="inner")
     test_df = pd.merge(mmlu_df, test_df["question_id"], on="question_id", how="inner")
 
     print("Dataframe samples")
-    print(easy_train_df.head())
-    print(mid_train_df.head())
-    print(hard_train_df.head())
+    print(train_df.head())
     print(test_df.head())
 
-    print(
-        f"Distribution of data (easy:mid:hard:test) = {len(easy_train_df)}:{len(mid_train_df)}:{len(hard_train_df)}:{len(test_df)}"
-    )
-
-    easy_train_ds = prepare_dataset(
-        tokenizer=tokenizer, get_sys_prompt=get_sys_prompt, get_user_prompt=get_user_prompt, df=easy_train_df
-    )
-    mid_train_ds = prepare_dataset(
-        tokenizer=tokenizer, get_sys_prompt=get_sys_prompt, get_user_prompt=get_user_prompt, df=mid_train_df
-    )
-    hard_train_ds = prepare_dataset(
-        tokenizer=tokenizer, get_sys_prompt=get_sys_prompt, get_user_prompt=get_user_prompt, df=hard_train_df
+    train_ds = prepare_dataset(
+        tokenizer=tokenizer, get_sys_prompt=get_sys_prompt, get_user_prompt=get_user_prompt, df=train_df
     )
     test_ds = prepare_dataset(
         tokenizer=tokenizer, get_sys_prompt=get_sys_prompt, get_user_prompt=get_user_prompt, df=test_df, mask_input=True
     )
 
     print("Dataset samples")
-    print(easy_train_ds[0])
+    print(train_ds[0])
     print(test_ds[0])
 
     tokenizer.pad_token = tokenizer.eos_token
@@ -148,76 +119,59 @@ def train_sft_curriculum(name, model_id, easy_train_df_path, mid_train_df_path, 
         tokenizer=tokenizer, padding=True, pad_to_multiple_of=8, return_tensors="pt"
     )
 
-    base_output_dir = Path(__file__).parent.joinpath("../../../artifacts/sft_curriculum").joinpath(name)
+    output_dir = Path(__file__).parent.joinpath("../../../artifacts/sft_curriculum").joinpath(output_subpath)
 
-    easy_output_dir = base_output_dir.joinpath("easy")
-    mid_output_dir = base_output_dir.joinpath("mid")
-    hard_output_dir = base_output_dir.joinpath("hard")
-
-    def create_trainer(model, output_dir, train_ds, num_train_epochs, eval_on_start=False):
-        training_args = TrainingArguments(
-            seed=42,
-            data_seed=42,
-            output_dir=str(output_dir),
-            num_train_epochs=num_train_epochs,
-            per_device_train_batch_size=BATCH_SIZE,
-            per_device_eval_batch_size=BATCH_SIZE,
-            bf16=True,
-            bf16_full_eval=True,
-            logging_strategy="epoch",
-            eval_strategy="epoch",
-            report_to="none",
-            save_strategy="epoch",
-            overwrite_output_dir=True,
-            save_total_limit=1,
-            save_only_model=True,
-            eval_on_start=eval_on_start,
-        )
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            train_dataset=train_ds,
-            eval_dataset=test_ds,
-            data_collator=data_collator,
-            compute_metrics=compute_metrics,
-            preprocess_logits_for_metrics=preprocess_logits_for_metrics,
-        )
-        return trainer
 
     model = AutoModelForCausalLM.from_pretrained(model_id, device_map=DEVICE_MAP)
     inferred_device_map = model.hf_device_map
     print("\nInferred Device Map:", inferred_device_map)
 
-    trainer = create_trainer(
-        model=model, output_dir=easy_output_dir, train_ds=easy_train_ds, num_train_epochs=3, eval_on_start=True
+    training_args = TrainingArguments(
+        seed=42,
+        data_seed=42,
+        output_dir=str(output_dir),
+        num_train_epochs=num_train_epochs,
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
+        bf16=True,
+        bf16_full_eval=True,
+        logging_strategy="epoch",
+        eval_strategy="epoch",
+        report_to="none",
+        save_strategy="epoch",
+        overwrite_output_dir=True,
+        save_total_limit=1,
+        save_only_model=True,
+        eval_on_start=eval_on_start,
     )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_ds,
+        eval_dataset=test_ds,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+        preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+    )
+
     trainer.train()
 
-    # Otherwise, repeated training causes CUDA OOM
-    del model
-    del trainer
+    return get_last_checkpoint_dir(output_dir)
+
+
+def train_sft_curriculum(name, model_id, easy_train_df_path, mid_train_df_path, hard_train_df_path, test_df_path):
+    np.random.seed(42)
+    torch.manual_seed(42)
+
+    print(f"Using device: {DEVICE_MAP}")
+
+    easy_output_dir = train_sft_curriculum_stage(output_subpath=f"{name}/easy", model_id=model_id, train_df_path=easy_train_df_path, test_df_path=test_df_path, num_train_epochs=3, eval_on_start=True)
     cleaup()
 
-    model = AutoModelForCausalLM.from_pretrained(get_last_checkpoint_dir(easy_output_dir), device_map=DEVICE_MAP)
-    inferred_device_map = model.hf_device_map
-    print("\nInferred Device Map:", inferred_device_map)
-
-    trainer = create_trainer(model=model, output_dir=mid_output_dir, train_ds=mid_train_ds, num_train_epochs=3)
-    trainer.train()
-
-    del model
-    del trainer
+    mid_output_dir = train_sft_curriculum_stage(output_subpath=f"{name}/mid", model_id=easy_output_dir, train_df_path=mid_train_df_path, test_df_path=test_df_path, num_train_epochs=3, eval_on_start=False)
     cleaup()
 
-    model = AutoModelForCausalLM.from_pretrained(get_last_checkpoint_dir(mid_output_dir), device_map=DEVICE_MAP)
-    inferred_device_map = model.hf_device_map
-    print("\nInferred Device Map:", inferred_device_map)
-
-    trainer = create_trainer(model=model, output_dir=hard_output_dir, train_ds=hard_train_ds, num_train_epochs=4)
-    trainer.train()
-
-    del model
-    del trainer
+    hard_output_dir = train_sft_curriculum_stage(output_subpath=f"{name}/mid", model_id=mid_output_dir, train_df_path=mid_train_df_path, test_df_path=test_df_path, num_train_epochs=3, eval_on_start=False)
     cleaup()
 
-    return get_last_checkpoint_dir(hard_output_dir)
+    return hard_output_dir
