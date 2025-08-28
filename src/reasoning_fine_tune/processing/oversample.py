2f4167f41261e4d1c43eabca319732d64344fce4
aigoncharov <andrey@goncharov.page>
2025-08-28 21:02:06 +0100

    Add oversampling (paraphrasing)

---
+++ src/reasoning_fine_tune/processing/oversample.py
@@ -0,0 +1,131 @@
+import re
+
+import pandas as pd
+from tqdm import tqdm
+
+from reasoning_fine_tune.prompts.mmlu_single_token_answer import single_token_answer_prompt
+from reasoning_fine_tune.prompts.oversmaple import (
+    estimate_response_rating_system_prompt,
+    oversample_system_prompt,
+    valid_ratings,
+)
+from reasoning_fine_tune.utils.mistral_api_client import MistralAPIClient
+
+
+def estimate_rating(api_client: MistralAPIClient, question, answer):
+    chat_response = api_client.query_model(
+        [
+            {
+                "role": "system",
+                "content": estimate_response_rating_system_prompt,
+            },
+            {
+                "role": "user",
+                "content": f"""
+                [Instructions for Assistant]
+                {oversample_system_prompt}
+                [End of Instructions for Assistant]
+
+                [Question]
+                {question}
+                [End of Question]
+
+                [The Start of Assistant’s Answer]
+                {answer}
+                [The End of Assistant’s Answer]
+
+                You must rate the assistant's response on a scale of 1 to 10 (where 10 is the best and 1 is the worst) by strictly following this format: "[[rating]]", for example:"Rating: [[6]]"
+                """,
+            },
+        ]
+    )
+    response = chat_response.choices[0].message.content
+    # print(response)
+
+    rating = re.search("\\[\\[(\\d+?)\\]\\]", response).group(1)
+    # print(rating)
+    rating_int = int(rating)
+    assert rating_int in valid_ratings
+    return rating_int
+
+
+def paraphrase_with_model(api_client: MistralAPIClient, question):
+    chat_response = api_client.query_model(
+        [
+            {
+                "role": "system",
+                "content": oversample_system_prompt,
+            },
+            {
+                "role": "user",
+                "content": f"""
+                [Question Start]
+                {question}
+                [Question End]
+                """,
+            },
+        ]
+    )
+    response = chat_response.choices[0].message.content
+    return response
+
+
+def oversample_dataset(
+    in_filename,
+    out_filename,
+    question_col: str,
+    get_options_from_row,
+    oversample_ratio: int,
+    threshold=9,
+    dump_every=100,
+):
+    in_df = pd.read_csv(
+        in_filename,
+        sep="\t",
+        header=0,
+    )
+    out_df = pd.DataFrame(columns=in_df.columns)
+
+    mistral_api_client = MistralAPIClient()
+
+    invalid_entries = 0
+
+    mistral_api_client.reset_api_limits()
+
+    for index, row in tqdm(in_df.iterrows(), total=in_df.shape[0]):
+        question = single_token_answer_prompt(row[question_col], get_options_from_row(row))
+
+        for _ in range(oversample_ratio):
+            paraphrased_question = paraphrase_with_model(mistral_api_client, question)
+            mistral_api_client.wait()
+
+            try:
+                rating = estimate_rating(
+                    mistral_api_client,
+                    question,
+                    paraphrased_question,
+                )
+                if rating < threshold:
+                    raise Exception("Response rating is too low")
+            except:
+                invalid_entries += 1
+
+            new_row = row.copy()
+            new_row[question_col] = paraphrased_question
+            out_df = pd.concat([out_df, pd.DataFrame([new_row])], ignore_index=True)
+
+            mistral_api_client.wait()
+
+        iterations = index * oversample_ratio
+        if iterations % dump_every == 0:
+            out_df.to_csv(out_filename, sep="\t", index=False)
+            total_hits = 0
+            for value in mistral_api_client.api_limit_hits_by_client_ids.values():
+                total_hits += value
+            print(f"Over {iterations} iterations we hit {total_hits} API limits")
+
+    out_df.to_csv(out_filename, sep="\t", index=False)
+    print(
+        f"Processed dataset {out_filename}. Total entries: {in_df.shape[0] * oversample_ratio}. Invalid entries: {invalid_entries}."
+    )
+    return out_df
---
+++ src/reasoning_fine_tune/prompts/oversmaple.py
@@ -0,0 +1,4 @@
+oversample_system_prompt = "Below is a question with answer options. You are an expert in the topic of the question. Please rephrase the question in a way that still keeps the answer options valid. Output only he paraphrased question."
+
+valid_ratings = list(range(1, 11, 1))
+estimate_response_rating_system_prompt = 'Please act as an impartial judge and evaluate the quality of the response provided by an AI assistant to the user request displayed below. Your evaluation should consider factors such as the following all the settings in the system prompt, correspondences to the context of the user, the helpfulness, relevance and accuracy. Begin your evaluation by providing a short explanation. Be as objective as possible. After providing your explanation, you must rate the response on a scale of 1 to 10 (where 10 is the best and 1 is the worst) by strictly following this format: "[[rating]]", for example:"Rating: [[6]]".'
