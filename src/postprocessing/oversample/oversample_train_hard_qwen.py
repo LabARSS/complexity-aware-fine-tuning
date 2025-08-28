38053e47fdc4b355bea9c0c377415b75acad0a0b
aigoncharov <andrey@goncharov.page>
2025-08-28 21:02:06 +0100

    Add oversampling (paraphrasing)

---
+++ src/postprocessing/oversample/oversample_train_hard_qwen.py
@@ -0,0 +1,14 @@
+import ast
+from pathlib import Path
+
+from reasoning_fine_tune.processing.oversample import oversample_dataset
+
+oversample_dataset(
+    in_filename=Path(__file__).parent.joinpath("../../../data/data_splits/qwen/train_df_hard.tsv").resolve(),
+    out_filename=Path(__file__)
+    .parent.joinpath("../../../data/data_splits/qwen/train_df_hard_oversampled.tsv")
+    .resolve(),
+    question_col="question",
+    get_options_from_row=lambda row: ast.literal_eval(row["options"]),
+    oversample_ratio=5,
+)
