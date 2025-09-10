df315fccfa1ac1985c7c664d9934d8ba0f22af7e
aigoncharov <andrey@goncharov.page>
2025-09-10 10:24:09 +0200

    Add code for SFT baseline

---
+++ src/experiments/pipeline/sft_baseline/sft_baseline_phi4mini.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from reasoning_fine_tune.training.sft.config import TrainConfig
+from reasoning_fine_tune.training.sft.train import Trainer
+
+if __name__ == "__main__":
+    cfg = TrainConfig.preset("microsoft/Phi-4-mini-instruct")
+    cfg.epochs = 20
+    cfg.debug = False
+
+    cfg.save_dir = str(Path(__file__).parent.joinpath("../../../../artifacts/pipeline_20epochs/sft_baseline/phi4"))
+    cfg.train_path = str(
+        Path(__file__).parent.joinpath("../../../../data/data_splits/entropy_fallback/phi/train_df_combined.tsv")
+    )
+    cfg.valid_path = str(
+        Path(__file__).parent.joinpath("../../../../data/data_splits/entropy_fallback/phi/valid_df_combined.tsv")
+    )
+    cfg.test_path = str(
+        Path(__file__).parent.joinpath(
+            "../../../../data/data_splits/entropy_fallback/phi/test_balanced_combined_entr.tsv"
+        )
+    )
+    cfg.test_balanced_path = str(
+        Path(__file__).parent.joinpath(
+            "../../../../data/data_splits/entropy_fallback/phi/test_balanced_combined_entr.tsv"
+        )
+    )
+
+    trainer = Trainer(cfg)
+    trainer.train()
