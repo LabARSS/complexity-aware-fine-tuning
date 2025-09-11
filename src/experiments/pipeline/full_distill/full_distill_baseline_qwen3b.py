8185728d8d6e82c1f5380f98858502f91d54202b
aigoncharov <andrey@goncharov.page>
2025-09-11 14:50:32 +0200

    Join splits with distill data

---
+++ src/experiments/pipeline/full_distill/full_distill_baseline_qwen3b.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from reasoning_fine_tune.training.sft.config import TrainConfig
+from reasoning_fine_tune.training.sft.train import Trainer
+
+if __name__ == "__main__":
+    cfg = TrainConfig.preset("Qwen/Qwen2.5-3B-Instruct")
+    cfg.epochs = 20
+    cfg.debug = False
+    cfg.use_cot = True
+
+    cfg.save_dir = str(
+        Path(__file__).parent.joinpath("../../../../artifacts/pipeline_20epochs/full_distill_baseline/qwen")
+    )
+    cfg.train_path = str(
+        Path(__file__).parent.joinpath("../../../../data/data_splits/entropy_fallback/qwen/train_df_combined.tsv")
+    )
+    cfg.valid_path = str(
+        Path(__file__).parent.joinpath("../../../../data/data_splits/entropy_fallback/qwen/valid_df_combined.tsv")
+    )
+    cfg.test_path = str(
+        Path(__file__).parent.joinpath(
+            "../../../../data/data_splits/entropy_fallback/qwen/test_balanced_combined_entr.tsv"
+        )
+    )
+    cfg.test_balanced_path = str(
+        Path(__file__).parent.joinpath(
+            "../../../../data/data_splits/entropy_fallback/qwen/test_balanced_combined_entr.tsv"
+        )
+    )
+
+    trainer = Trainer(cfg)
+    trainer.train()
