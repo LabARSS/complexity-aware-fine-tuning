8d74771458996410c72124256784abe400f6082f
aigoncharov <andrey@goncharov.page>
2025-11-07 13:04:27 +0000

    Fix training args for A100

--- src/experiments/sft_by_complexity_splits/sft_by_complexity_splits_qwen3b.py
+++ src/experiments/sft_by_complexity_splits/sft_by_complexity_splits_qwen3b.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
         model_id="Qwen/Qwen2.5-3B-Instruct",
         use_lora=True,
         training_kwargs={
-            "per_device_train_batch_size": 16,
-            "per_device_eval_batch_size": 16,
-            "gradient_accumulation_steps": 4,
+            "per_device_train_batch_size": 8,
+            "per_device_eval_batch_size": 8,
+            "gradient_accumulation_steps": 8,
             "learning_rate": 2e-4,
             "lr_scheduler_type": "cosine",
             "warmup_ratio": 0.03,
