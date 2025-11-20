892528dcea4c4ed857b3c33c6a9a095d024e5a17
Andrey Goncharov <andrey@faillearnrepeat.net>
2025-11-20 12:09:31 +0000

    Add full distill config for gsm8k

---
+++ src/experiments/pipeline_gsm8k/full_distill/full_distill_baseline_phi4mini.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from reasoning_fine_tune.training.sft.config import TrainConfig
+from reasoning_fine_tune.training.sft.train import Trainer
+
+if __name__ == "__main__":
+    cfg = TrainConfig.preset("microsoft/Phi-4-mini-instruct")
+    cfg.epochs = 20
+    cfg.debug = False
+    cfg.use_cot = True
+    cfg.eval_validation_period = 1
+    cfg.eval_test_period = 0
+    cfg.run_eval_on_start = False
+
+    cfg.save_dir = str(
+        Path(__file__).parent.joinpath("../../../../artifacts/pipeline_gsm8k/full_distill_baseline/phi4")
+    )
+    cfg.train_path = str(Path(__file__).parent.joinpath("../../../../data/out/distillation/gsm8k_distilled_cot.jsonl"))
+    cfg.valid_path = str(Path(__file__).parent.joinpath("../../../../data/source/gsm8k/gsm8k_test.jsonl"))
+    cfg.test_path = str(Path(__file__).parent.joinpath("../../../../data/source/gsm8k/gsm8k_test.jsonl"))
+    cfg.test_balanced_path = str(Path(__file__).parent.joinpath("../../../../data/source/gsm8k/gsm8k_test.jsonl"))
+
+    trainer = Trainer(cfg)
+    trainer.train()
---
+++ src/experiments/pipeline_gsm8k/pipeline/pipeline_pt2_phi4mini.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from reasoning_fine_tune.training.sft.config import TrainConfig
+from reasoning_fine_tune.training.sft.train import Trainer
+
+if __name__ == "__main__":
+    # TODO: Fix me
+    cfg = TrainConfig.preset("microsoft/Phi-4-mini-instruct")
+    cfg.epochs = 10
+    cfg.debug = False
+    cfg.base_model = str(Path(__file__).parent.joinpath("../../../../artifacts/pipeline_20epochs/pipeline/phi4/pt1"))
+    cfg.use_cot = True
+    cfg.eval_validation_period = 0
+    cfg.eval_test_period = 10
+    cfg.run_eval_on_start = False
+    cfg.batch_size = 2
+    cfg.eval_batch_size = 8
+
+    cfg.save_dir = str(Path(__file__).parent.joinpath("../../../../artifacts/pipeline_20epochs/pipeline/phi4/pt2"))
+    cfg.train_path = str(
+        Path(__file__).parent.joinpath("../../../../data/data_splits/entropy_fallback/phi/train_df_hard.tsv")
+    )
+    cfg.valid_path = str(
+        Path(__file__).parent.joinpath("../../../../data/data_splits/entropy_fallback/phi/valid_df_combined.tsv")
+    )
+    cfg.test_path = str(
+        Path(__file__).parent.joinpath(
+            "../../../../data/data_splits/entropy_fallback/phi/test_balanced_combined_entr.tsv"
+        )
+    )
+    cfg.test_balanced_path = str(
+        Path(__file__).parent.joinpath(
+            "../../../../data/data_splits/entropy_fallback/phi/test_balanced_combined_entr.tsv"
+        )
+    )
+
+    trainer = Trainer(cfg)
+    trainer.train()
