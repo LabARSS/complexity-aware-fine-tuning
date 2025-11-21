19c2031523e0ce5202f26d3fcbd009289a828938
Andrey Goncharov <andrey@faillearnrepeat.net>
2025-11-21 14:14:43 +0000

    Add new entropy exp

--- src/reasoning_fine_tune/prompts/gsm8k_cot_answer.py
+++ src/reasoning_fine_tune/prompts/gsm8k_cot_answer.py
@@ -8,6 +8,11 @@ def cot_sys_prompt_from_row(row: pd.Series):
     return sys_msg
 
 
+def single_token_sys_prompt_from_row(row: pd.Series):
+    sys_msg = f"The following are grade school math word problems. Write down only the correct answer and nothing else by strictly following this format: {answer_marker[0]}correct_answer{answer_marker[1]}."
+    return sys_msg
+
+
 def cot_answer_prompt_from_row(row: pd.Series) -> str:
     question = row["question"]
     assert isinstance(question, str)
